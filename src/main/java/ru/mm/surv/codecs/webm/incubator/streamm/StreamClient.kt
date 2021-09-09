ac327dcb288604f5aa28e6223b5f72118bb32822
Mikhail Mineev <mikevbif@yandex.ru>
2021-09-09 19:24:17 +0300

    some kotlin

--- src/main/java/ru/mm/surv/codecs/webm/incubator/streamm/StreamClient.kt
+++ src/main/java/ru/mm/surv/codecs/webm/incubator/streamm/StreamClient.kt
@@ -1,127 +1,97 @@
-package ru.mm.surv.codecs.webm.incubator.streamm;
+package ru.mm.surv.codecs.webm.incubator.streamm
 
-import ru.mm.surv.codecs.webm.util.stream.Buffer;
+import java.lang.RuntimeException
+import java.io.OutputStream
+import java.lang.Exception
 
-import java.io.IOException;
-import java.io.OutputStream;
+class StreamClient(private val stream: Stream, private val output: OutputStream) {
+    private var runs = true
+    private var requestedFragmentSequence = -1
+    private var sendSingleFragment = false
 
-public class StreamClient {
-
-    private final Stream stream;
-    private final OutputStream output;
-
-    private boolean runs = true;
-
-    private int requestedFragmentSequence = -1;
-
-    private boolean sendSingleFragment = false;
-
-
-    public StreamClient(Stream stream, OutputStream output) {
-        this.stream = stream;
-        this.output = output;
+    fun setRequestedFragmentSequence(requestedFragmentSequence: Int) {
+        this.requestedFragmentSequence = requestedFragmentSequence
     }
 
-    public void setRequestedFragmentSequence(int requestedFragmentSequence) {
-        this.requestedFragmentSequence = requestedFragmentSequence;
+    fun setSendSingleFragment(sendSingleFragment: Boolean) {
+        this.sendSingleFragment = sendSingleFragment
     }
 
-    public void setSendSingleFragment(boolean sendSingleFragment) {
-        this.sendSingleFragment = sendSingleFragment;
-    }
-
-
-    public void run() throws InterruptedException, IOException {
-
-        try {
+    fun run() {
+        output.use { output ->
             // report the starting of this client
-            stream.postEvent(new ServerEvent(ServerEvent.CLIET_START));
+            stream.postEvent(ServerEvent(ServerEvent.CLIET_START))
 
             // waiting for header
-            byte[] header = null;
+            var header: ByteArray? = null
             while (header == null) {
-                if (stream.isRunning()) {
-                    return;
+                if (stream.isRunning) {
+                    return
                 }
-
-                Thread.sleep(200);
-
-                header = stream.getHeader();
+                Thread.sleep(200)
+                header = stream.header
             }
 
             // writing header
             try {
-                output.write(header);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                output.write(header)
+            } catch (e: Exception) {
+                throw RuntimeException(e)
             }
 
             /* Web browsers may try to detect some features of the server and open
              * multiple connections. They may close the connection eventually so
              * we wait for a little bit to ensure this request is serious. (And the
              * client needs more than just the header).
-             */
-
-            Thread.sleep(1000);
+             */Thread.sleep(1000)
 
             // sending fragments
-            int nextSequence = requestedFragmentSequence;
-            while (runs && stream.isRunning()) {
-
-                boolean fragmentSent = false;
+            var nextSequence = requestedFragmentSequence
+            while (runs && stream.isRunning) {
+                var fragmentSent = false
 
                 // while there is a new fragment is available
-                int streamAge;
+                var streamAge = stream.fragmentAge
                 while (runs
-                        && stream.isRunning()
-                        && nextSequence <= (streamAge = stream.getFragmentAge())) {
+                        && stream.isRunning
+                        && nextSequence <= stream.fragmentAge.also { streamAge = it }) {
 
                     // notification if a fragment was skipped
-                    if (nextSequence > 0 && streamAge - nextSequence > 1)
-                        stream.postEvent(new ServerEvent(ServerEvent.CLIET_FRAGMENT_SKIP));
-
-                    nextSequence = streamAge + 1;
+                    if (nextSequence > 0 && streamAge - nextSequence > 1) stream.postEvent(ServerEvent(ServerEvent.CLIET_FRAGMENT_SKIP))
+                    nextSequence = streamAge + 1
 
                     // getting current movie fragment
-                    MovieFragment fragment = stream.getFragment();
+                    val fragment = stream.fragment
 
                     // send the fragment data to the client
                     try {
-
-                        Buffer[] buffers = fragment.getBuffers();
-                        for (Buffer buffer : buffers) {
+                        val buffers = fragment!!.getBuffers()
+                        for (buffer in buffers) {
 
                             // writing data packet
-                            output.write(buffer.getData(), buffer.getOffset(), buffer.getLength());
-
+                            output.write(buffer.data, buffer.offset, buffer.length)
                         }
-
-                        fragmentSent = true;
+                        fragmentSent = true
 
                         // only one fragment requested:
                         if (sendSingleFragment) {
-                            runs = false;
+                            runs = false
                         }
-
-                    } catch (Exception e) {
+                    } catch (e: Exception) {
 
                         // closed connection
-                        runs = false;
+                        runs = false
                     }
                 }
 
                 // currently no new fragment, sleeping 200 ms
                 if (!fragmentSent) {
-                    Thread.sleep(200);
+                    Thread.sleep(200)
                 }
-
             }
 
             // report the stopping of thes client
-            stream.postEvent(new ServerEvent(ServerEvent.CLIET_STOP));
-        } finally {
-            output.close();
+            stream.postEvent(ServerEvent(ServerEvent.CLIET_STOP))
         }
     }
-
 }
